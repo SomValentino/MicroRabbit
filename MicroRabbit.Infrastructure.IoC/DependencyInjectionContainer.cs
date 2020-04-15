b086bd5651271c0b90f7de7a1ea1501297baf2a4
Valentine <valazom@gmail.com>
2020-04-15 17:57:26 +0200

    initial commit of Banking MicroService

--- MicroRabbit.Infrastructure.IoC/DependencyInjectionContainer.cs
+++ MicroRabbit.Infrastructure.IoC/DependencyInjectionContainer.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infrastructure.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace MicroRabbit.Infrastructure.IoC
 {
-    public class DependencyInjectionContainer
+    public static class DependencyInjectionContainer
     {
-        public DependencyInjectionContainer()
+        public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddTransient<IEventBus, RabbitMQBus>();
         }
     }
 }
